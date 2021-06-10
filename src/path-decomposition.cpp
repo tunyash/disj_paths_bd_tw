b48d3c65fd741787d027e2a69b49b923edb6434c
elproblema <gribennikvitalij@gmail.com>
2021-06-10 17:05:51 +0300

    tw-exercise headers

---
+++ src/path-decomposition.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <vector>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <algorithm>
+#include "path-decomposition.h"
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+
+PathDecomposition::CorectnessException::CorectnessException(int error_type): _error_type(error_type) {}
+PathDecomposition::CorectnessException::CorectnessException
+        (int i, int j, int k, int u):
+        _error_type(INCONTINUITY), _bag_i(i), _bag_j(j), _bag_k(k), _violating_node(u) {}
+
+const char *PathDecomposition::CorectnessException::what() const throw() {
+    switch (_error_type) {
+        case EDGES:
+            return "Not all edges are in path-width decomposition";
+        case NEGATIVE_VERTICES:
+            return "Vertices in bags are not in [0; |V(_g)|)";
+        case INCONTINUITY:
+            return ("_bags["
+                    + std::to_string(_bag_i)
+                    + "] and _bags["
+                    + std::to_string(_bag_j)
+                    + "] contains "
+                    + std::to_string(_bag_k)
+                    + " but _bags["
+                    + std::to_string(_violating_node)
+                    + "] does not").c_str();
+        case VERTICES:
+            return "Not all vertices are in path-width decomposition";
+    }
+}
+
+bool PathDecomposition::Check() {
+    int n = boost::num_vertices(_g);
+
+    for (std::vector<int> bag : _bags) {
+        std::sort(bag.begin(), bag.end());
+        bag.resize(std::unique(bag.begin(), bag.end()) - bag.begin());
+        //Erasing non-unique vertices from bags
+        for (int v : bag) {
+            if (v < 0 || v >= n)
+                throw PathDecomposition::CorectnessException(NEGATIVE_VERTICES);
+        }
+    }
+
+    std::vector<int> sum(n);
+    std::vector<int> left(n, n + 1);
+    std::vector<int> right(n, -1);
+
+    for (int i = 0; i < _bags.size(); ++i) {
+        for (int v : _bags[i]) {
+            sum[v]++;
+            left[v] = std::min(left[v], i);
+            right[v] = std::max(right[v], i);
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (sum[i] == 0) throw PathDecomposition::CorectnessException(VERTICES);
+        if (right[i] - left[i] + 1 != sum[i]) {
+            int j = left[i];
+            for (;
+                    std::find(_bags[j].begin(), _bags[j].end(), i) != _bags[j].end();
+                    ++j);
+            throw PathDecomposition::CorectnessException(left[i], j, right[i], i);
+        }
+    }
+
+    Graph path_g(n);
+
+    for (std::vector<int> bag : _bags) {
+        for (int v : bag) {
+            for (int u : bag) {
+                if (u == v) continue;
+                boost::add_edge(u, v, path_g);
+            }
+        }
+    }
+
+    for (int v = 0; v < n; ++v) {
+        for (auto it = boost::adjacent_vertices(v, _g).first; it != boost::adjacent_vertices(v, _g).second; ++it) {
+            if (std::find(boost::adjacent_vertices(v, path_g).first, boost::adjacent_vertices(v, path_g).second, *it) ==
+                boost::adjacent_vertices(v, path_g).second) throw PathDecomposition::CorectnessException(EDGES);
+        }
+    }
+}
+
+PathDecomposition::PathDecomposition(std::vector<std::vector<int>> bags, Graph g): _bags(bags), _g(g) {
+    Check();
+}
---
+++ src/path-decomposition.h
@@ -0,0 +1,63 @@
+#include <vector>
+#include <string>
+#include <boost/graph/adjacency_list.hpp>
+
+class PathDecomposition {
+
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> Graph;
+
+    std::vector<std::vector<int>> _bags;
+    Graph _g;
+
+public:
+
+    enum error_types {
+        EDGES = 0,
+        NEGATIVE_VERTICES = 1,
+        INCONTINUITY = 2,
+        VERTICES = 3
+    };
+
+    struct CorectnessException : public std::exception{
+        /*
+        Thrown when path-width decomposition is incorrect
+        */
+    private:
+    public:
+        int _error_type;
+        int _bag_i, _bag_j, _bag_k, _violating_node;
+
+        CorectnessException(int error_type);
+        /*
+        @error_type == 0 used when in graph |_g| exists edge (v, u) but in |_bags| there is no bag that contains {v, u}
+        @error_type == 1 used when in |_bags| exists vertex |v| that does not belong to [0; |V(_g)|)
+        @error_type == 3 used when exists vertex |v| that does not belong to |_bags|
+        */
+        CorectnessException(int i, int j, int k, int u);
+        /*
+        This is used only for error_type == 2
+        @error_type == 2 used when _bags[i] and _bags[k] contains vertex |v| but _bags[j] does not.
+        i < j < k
+        */
+        ~CorectnessException() throw() {}
+        const char* what() const throw();
+    };
+
+    PathDecomposition(std::vector<std::vector<int>> bags, Graph g);
+    /*
+    Constructor creates path-width decomposition where |bags[i]| is i-th bag of decomposition.
+    |bags| is path-width decompostion of graph |g|.
+    @exceptions throw CorrectnessException if decomposition is not correct
+    */
+
+private:
+
+    bool Check();
+    /*
+        Function checks correctness of path-width decomposition |_bags| of graph |_g|.
+        @return true if decomposition is correct false otherwise
+    */
+
+
+};
+
