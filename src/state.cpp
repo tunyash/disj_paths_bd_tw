897f05016374e3a1f000ca129425594610cfcdea
bazillo <zoro_k@mail.ru>
2021-07-07 12:45:46 +0300

    dp parameters added

--- src/state.cpp
+++ src/state.cpp
@@ -1,37 +1,50 @@
 #include "state.h"
 
+Parameters::Parameters(std::vector<vertex_t> s, std::vector<vertex_t> t, int n) : s(s), t(t) {
+    color.resize(n, -1);
+    for (int i = 0; i < s.size(); i++)
+        color[s[i]] = i, color[s[t[i]]] = i;
+}
+
 vertex_t State::phi(vertex_t v) {
-    if (available.count(v)) return color[v];
+    if (available.count(v)) return end[v];
     return -1;
 }
 
-State::State(std::set<vertex_t> available, std::map<vertex_t, int> color) : available(available),
-                                                                            color(color) {}
+State::State(std::set<vertex_t> available, std::map<vertex_t, int> end, int complete_paths,
+             const Parameters &parameters) :
+        available(available),
+        end(end),
+        complete_paths(complete_paths),
+        parameters(parameters) {}
 
-std::vector<State> State::calculate(NicePathDecomposition::Bag bag) {
+
+std::vector<State> State::calculate(const NicePathDecomposition::Bag &bag) {
     std::set<vertex_t> new_available = available;
-    std::map<vertex_t, int> new_color = color;
+    std::map<vertex_t, int> new_end = end;
     switch (bag.type) {
         case NicePathDecomposition::ADD_VERTEX:
-            // just add vertex to available
             new_available.insert(bag.vertex);
-            new_color[bag.vertex] = bag.vertex;
-            return {State(new_available, new_color)};
+            new_end[bag.vertex] = bag.vertex;
+            return {State(new_available, new_end, complete_paths, parameters)};
         case NicePathDecomposition::REMOVE_VERTEX:
-            // delete vertex from available
             new_available.erase(bag.vertex);
-            return {State(new_available, new_color)};
+            return {State(new_available, new_end, complete_paths, parameters)};
         case NicePathDecomposition::ADD_EDGE:
-            std::vector<State> states = {State(new_available, new_color)};
+            std::vector<State> states = {State(new_available, new_end, complete_paths, parameters)};
             vertex_t v = bag.edge.m_source, u = bag.edge.m_target;
-            // if one of vertexes is unavailable we can't change state
             if (!available.count(v) || !available.count(u))
                 return states;
             vertex_t end1 = phi(v), end2 = phi(u);
-            if (end1 != v) new_available.erase(v);
-            if (end2 != u) new_available.erase(u);
-            new_color[end1] = end2, new_color[end2] = end1;
-            states.push_back(State(new_available, new_color));
+            bool complete_path = false;
+            if (parameters.color[end1] != -1 && parameters.color[end2] != -1) {
+                if (parameters.color[end1] != parameters.color[end2]) return states;
+                complete_path = true;
+            }
+            if (end1 != v || parameters.color[v] != -1) new_available.erase(v);
+            if (end2 != u || parameters.color[u] != -1) new_available.erase(u);
+            new_end[end1] = end2, new_end[end2] = end1;
+            states.emplace_back(new_available, new_end, complete_path + complete_path, parameters);
             return states;
     }
 }
--- src/state.h
+++ src/state.h
@@ -3,21 +3,35 @@
 
 #include "nice-path-decomposition.h"
 
+struct Parameters {
+    Parameters(std::vector<vertex_t> s, std::vector<vertex_t> t, int n);
+
+    std::vector<vertex_t> s, t;
+    std::vector<int> color;
+    // color[v] = x ∈ [-1,|s|) : color[s[i]] = i, color[t[i]] = i, other color[v] = -1
+};
+
 class State {
     std::set<vertex_t> available;
     // ends of paths or vertices out from paths
 
-    std::map<vertex_t,int> color;
-    // color helps to find phi;
+    std::map<vertex_t, int> end;
+    // keeps another end of path;
+
+    const Parameters parameters;
 
 public:
-    std::vector<State> calculate(NicePathDecomposition::Bag bag);
+    int complete_paths = 0;
+
+    std::vector<State> calculate(const NicePathDecomposition::Bag &bag);
     // returns states in which we can go with this bag
 
-    State(std::set<vertex_t>available,std::map<vertex_t,int> color);
+    State(std::set<vertex_t> available, std::map<vertex_t, int> color, int complete_paths,
+          const Parameters &parameters);
 
     vertex_t phi(vertex_t v);
     // (end of path) -> (another end of path)
+
 };
 
 
